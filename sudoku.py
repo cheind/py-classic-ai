24d62eed921b039fc5ff85ba40f1091561362869
Christoph Heindl <christoph.heindl@gmail.com>
2017-11-27 17:53:17 +0100

    allow for rare backtracking required when only few cells are given

--- sudoku.py
+++ sudoku.py
@@ -3,6 +3,8 @@ __copyright__ = 'Copyright 2017'
 __license__ = 'BSD'
 
 import numpy as np
+import random
+from collections import namedtuple
 
 board = np.array([
     0, 0, 9, 4, 8, 0, 0, 0, 0,
@@ -16,6 +18,9 @@ board = np.array([
     0, 5, 3, 0, 0, 6, 9, 0, 0,
 ], dtype='b').reshape((9,9))
 
+
+Move = namedtuple('Move', 'choices row col', verbose=False)
+
 def possibilities(board, row, col):
     i = row // 3
     j = col // 3
@@ -26,13 +31,19 @@ def possibilities(board, row, col):
 
 def solve(board):
     rows, cols = np.where(board == 0)
-    moves = [(possibilities(board, r, c), r, c) for r,c in zip(rows, cols)]
-    moves.sort(key=lambda x:x[0].shape[0])
-
+    moves = [Move(choices=possibilities(board, r, c), row=r, col=c) for r,c in zip(rows, cols)]
+    
     if len(moves) == 0:
         return board
-
-    board[moves[0][1], moves[0][2]] = moves[0][0][0]
-    return solve(board)
+    
+    m = min(moves, key=lambda x:x.choices.shape[0])
+    for c in m.choices:
+        b = board.copy()
+        b[m.row, m.col] = c
+        b = solve(b)
+        if b is not None:
+            return b
+        print('need to backtrack...') # rare but possible if few fields are known from the beginning.
+    return None
 
 print(solve(board))
