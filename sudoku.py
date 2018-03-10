98e89feabff5f0343a4a4c31cd2fda93288333ed
Christoph Heindl <christoph.heindl@gmail.com>
2018-03-10 08:59:53 +0100

    adding more strategies

--- sudoku.py
+++ sudoku.py
@@ -7,48 +7,137 @@ import random
 from collections import namedtuple
 
 board = np.array([
-    0, 0, 9, 4, 8, 0, 0, 0, 0,
-    0, 0, 0, 0, 6, 3, 2, 0, 0,
-    0, 0, 6, 0, 7, 0, 4, 3, 8,
-    0, 0, 0, 0, 0, 0, 7, 0, 6,
-    5, 9, 0, 0, 0, 7, 0, 0, 0, 
-    7, 0, 8, 0, 4, 9, 0, 1, 3, 
-    0, 0, 0, 0, 0, 0, 6, 8, 7,
-    0, 0, 0, 8, 9, 1, 0, 0, 0,
-    0, 5, 3, 0, 0, 6, 9, 0, 0,
+    0, 0, 1, 0, 0, 6, 3, 5, 0,
+    0, 0, 6, 5, 0, 8, 0, 0, 7,
+    2, 0, 0, 0, 0, 0, 0, 0, 8,
+    0, 0, 5, 0, 0, 0, 0, 9, 2,
+    0, 1, 0, 0, 5, 0, 0, 0, 0, 
+    6, 9, 0, 0, 0, 0, 0, 4, 0, 
+    0, 0, 0, 0, 0, 1, 0, 7, 6,
+    0, 0, 0, 3, 6, 0, 0, 0, 0,
+    4, 0, 0, 7, 0, 0, 1, 0, 0,
 ], dtype='b').reshape((9,9))
 
-Cell = namedtuple('Cell', 'choices row col', verbose=False)
+def init_state(board):
+    state = {
+        'board' : np.zeros((9,9), dtype='b'),
+        'taken' : np.zeros((9,9,9), dtype='b')
+    }
+    it = np.nditer(board, flags=['multi_index'])
+    while not it.finished:
+        if it[0] != 0:
+            update_state(state, it.multi_index[0], it.multi_index[1], it[0])
+        it.iternext()
+    return state
 
-def possibilities(board, row, col):
-    i = row // 3
-    j = col // 3
-    u = np.unique(np.concatenate((board[:, col], board[row, :], board[i*3:i*3+3, j*3:j*3+3].ravel())))
-    if u[0] == 0:
-        u = u[1:]
-    return np.delete([1, 2, 3, 4, 5, 6, 7, 8, 9], u-1)
+def clone_state(state):
+    return {
+        'board' : state['board'].copy(),
+        'taken' : state['taken'].copy(),
+    }
 
-def solve(board, order):
-    rows, cols = np.where(board == 0)
-    cells = [Cell(choices=possibilities(board, r, c), row=r, col=c) for r,c in zip(rows, cols)]
+
+def update_state(state, row, col, v):
+    state['board'][row, col] = v
     
-    if len(cells) == 0:
-        return board, order
+    i = (row // 3) * 3
+    j = (col // 3) * 3
+    state['taken'][row, col] = True             # all digits taken
+    state['taken'][row, :, v-1] = True          # v taken along row
+    state['taken'][:, col, v-1] = True          # v taken along col
+    state['taken'][i:i+3, j:j+3, v-1] = True    # v taken along subgrid
+
+    fillin(state)
+
+    return state
+
+def possibilities(state, row, col):    
+    avail = np.where(state['taken'][row, col] == 0)[0] + 1
+    if avail.shape[0] < 2:
+        return avail
     
-    m = min(cells, key=lambda x:x.choices.shape[0])
-    for c in m.choices: # rare need to loop over multiple choices, expect when few fields are known from the beginning.       
-        nextboard = board.copy()
-        nextorder = order.copy()
-        
-        nextboard[m.row, m.col] = c
-        nextorder[m.row, m.col] = np.max(order) + 1
-        
-        nextboard, nextorder = solve(nextboard, nextorder)
-        if nextboard is not None:  
-            return nextboard, nextorder      
-    return None, order
+    free = state['board'] == 0
+    mask = np.zeros((9,9), dtype=bool)
 
-board, order = solve(board, np.zeros((9,9), dtype=int))
+    def reducebyopt(mask, avail):
+        tk = state['taken'][mask & free]
+        tk = np.where(tk.all(0))[0] + 1
+        return np.intersect1d(avail, tk)
 
-print(board)
-print(order)
+    i = (row // 3) * 3
+    j = (col // 3) * 3    
+    mask[i:i+3, j:j+3] = True
+    mask[row, col] = False
+    ravail = reducebyopt(mask, avail)
+    if (ravail.shape[0] == 1):
+        return ravail
+
+    mask.fill(False)
+    mask[row, :] = True
+    mask[row, col] = False
+    ravail = reducebyopt(mask, avail)
+    if (ravail.shape[0] == 1):
+        return ravail
+
+    mask.fill(False)
+    mask[:, col] = True
+    mask[row, col] = False
+    ravail = reducebyopt(mask, avail)
+    if (ravail.shape[0] == 1):
+        return ravail
+
+    return avail
+
+def fillin(state):
+    for i in range(3):
+        for j in range(3):
+            sel = np.array(np.where(state['taken'][i*3:i*3+3,j*3:j*3+3].transpose((2,0,1)) == 0))
+
+            for k in range(9):
+                n = sel[1:, sel[0] == k]
+                if n.shape[1] < 2:
+                    continue
+                
+                if ((n[0,0]-n[0,:]) == 0).all():
+                    # along row
+                    state['taken'][i*3 + n[0,0], :, k] = True
+                    state['taken'][i*3 + n[0,:], j*3 + n[1,:], k] = False
+                elif ((n[1,0]-n[1,:]) == 0).all():
+                    # along col
+                    state['taken'][:, j*3 + n[1,0], k] = True
+                    state['taken'][i*3 + n[0,:], j*3 + n[1,:], k] = False
+                
+    
+
+
+def solve(state):
+    stack = [state]
+
+    while len(stack) > 0:
+        state = stack.pop()
+
+        rows, cols = np.where(state['board'] == 0)
+        cells = [(possibilities(state, r, c), r, c) for r,c in zip(rows, cols)]
+
+        if len(cells) == 0:
+            return state
+    
+        m = min(cells, key=lambda x:x[0].shape[0])
+        print(f'{m}')
+        if m[0].shape[0] == 1:
+            update_state(state, m[1], m[2], m[0][0])
+            stack.append(state)
+        else:          
+
+            print(state['board']) 
+            print(sorted(cells, key=lambda x: len(x[0])))
+            for c in m[0]: # rare need to loop over multiple choices, expect when few fields are known from the beginning.       
+                clone = clone_state(state)
+                update_state(clone, m[1], m[2], c)
+                stack.append(clone)
+
+                
+    return None
+
+
+print(solve(init_state(board)))
